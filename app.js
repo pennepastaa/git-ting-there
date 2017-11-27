f2c42013f713b84cd19ed47bd31c5449c91023db
Pennie Li <pennepastaa@gmail.com>
2017-11-27 15:32:10 +1100

    mon 3:31 card 4 done
    is a form but is able to input new messages to newsfeed and clear input box afterwards

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 
 function showMessage(){
-         var message = document.getElementById("message").value;
-         showmessage.innerHTML= message;
-       }
+
+    var msg = document.getElementById("message");
+    var post = document.getElementById("box");
+
+    var para = document.createElement("p");
+    var str = document.createTextNode(msg.value);
+
+    para.appendChild(str);
+    post.appendChild(para);
+    //box.innerHTML = (`${msg}`);
+    document.getElementById('sendmsgs').reset();
+    //document.getElementById('elementId').value='';
+}
+
+
          //console.log(`${message}`);
