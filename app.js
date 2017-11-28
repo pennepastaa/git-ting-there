a9e6b1776c0ee43f7fbe6fbe7bd46d982616700e
Pennie Li <pennepastaa@gmail.com>
2017-11-28 15:10:20 +1100

    Tues 3:10pm
    data saves to database
    DO NOT CLEAR FORM BEFORE DATA IS SAVED

--- app.js
+++ app.js
@@ -9,14 +9,24 @@ function showMessage(){
 
     para.appendChild(str);
     post.appendChild(para);
-    //box.innerHTML = (`${msg}`);
-    document.getElementById('sendmsgs').reset();
-    //document.getElementById('elementId').value='';
 
-    var firebaseRef = firebase.database().ref();
+//^sends messages to newsfeed and resets the form
 
-    firebaseRef.child("Text").set("some value")
+var firebaseRef = firebase.database().ref().child("messages");
+//var messageText = para.value;
+firebaseRef.push().set(msg.value);
+
+  document.getElementById('sendmsgs').reset();
 }
+
+Retrieving data
+var box = doument.getElementById("box");
+var firebaseBoxRef = firebase.database().ref().child("box");
+firebaseBoxRef.on('value', function(datasnapshot){
+ fireBoxheading.innerText = datasnapshot.val();
+ });
+
+
     //console.log(`${message}`);
 
  //window.alert("Congrats!!!!! you are our 100th visitor. Click here to win an iPhone X");
